8a1a35d004189ac91f14eb68acbe64aac6f1ae9a
root <aikanshgarg0303@gmail.com>
2018-12-09 19:58:44 +0530

    setup aos for animations

--- js/init.js
+++ js/init.js
@@ -1,5 +1,3 @@
-
-
 (function($){
   $(function(){
 
@@ -14,4 +12,26 @@ $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
+
+
+// putting at last so that materialize.js works
+
+// ScrollReveal library
+// ScrollReveal().reveal('.heya', { delay: 900, reset: true, duration: 4000, origin: 'top' });
+
+// ScrollReveal().reveal('#average-guy', { delay: 1000, reset: true });
+// ScrollReveal().reveal('#index-banner', { delay: 600 });
+
+// ScrollReveal().reveal('#info', { delay: 500, reset: true});
+// ScrollReveal().reveal('#box-out', { delay: 300, reset: true });
+
+// ScrollReveal().reveal('.carousel', { delay: 300, duration: 2000});
+// ScrollReveal().reveal('.responsive-img', { delay: 600});
+// ScrollReveal().reveal('.other-projects', { delay: 300});
+
+// ScrollReveal().reveal('.page-footer', { delay: 200});
+// ScrollReveal().reveal('.hand', { delay: 1500, scale: 0, reset: true, easing: 'cubic-bezier(1,0,0,1)', distance: '50px', duration: 950});
+
+
+// AOS.init();
 
