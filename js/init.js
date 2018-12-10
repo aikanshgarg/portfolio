dd0a8d9f76a556d402907478ea9c4aac70806380
root <aikanshgarg0303@gmail.com>
2018-12-11 01:01:00 +0530

    Typewriter: check!

--- js/init.js
+++ js/init.js
@@ -31,3 +31,76 @@ $('.carousel.carousel-slider').carousel({
 
 // ScrollReveal().reveal('.page-footer', { delay: 200});
 // ScrollReveal().reveal('.hand', { delay: 1500, scale: 0, reset: true, easing: 'cubic-bezier(1,0,0,1)', distance: '50px', duration: 950});
+
+
+
+// new TypeIt('#stay', {
+//   speed: 150,
+//  // autoStart: true,
+//   cursor: false,
+//   startDelay: 200,
+//   loop: true
+// });
+
+// new TypeIt('#after-stay', {
+//   speed: 150,
+//   //autoStart: true,
+//   cursor: false,
+//   startDelay: 5800
+// });
+
+// new TypeIt('#nostay', {
+//   speed: 50,
+//   breakLines: false,
+//   //autoStart: true,
+//   //startDelay: 8000,
+//   //loop: true
+// }).type("whats this?");
+
+
+
+// --------------LOOP:true NOT WORKING------------
+
+new TypeIt('#nostay', {
+  speed: 100,
+  startDelay: 600,
+  //loop: true
+}).type('playing with CSS.')
+.pause(600)
+.options({speed: 200})
+.delete(17)
+.options({speed: 45})
+.pause(300)
+.type('JavaScrilt')
+.pause(600)
+.options({speed: 200})
+.delete(2)
+.options({speed: 45})
+.pause(300)
+.type('pt.')
+.pause(400)
+.options({speed: 200})
+.delete(24)
+.type('Ruby on Rails.')
+.pause(500)
+.options({speed: 45})
+.delete(14)
+.options({speed: 45})
+.pause(900)
+.type('Codeeng')
+.pause(800)
+.options({speed: 200})
+.delete(4)
+.options({speed: 200})
+.pause(300)
+.type('ing.')
+// .pause(600)
+// .options({speed: 100})
+// .delete(24)
+// .type('Zumba.')
+// .pause(600)
+// .options({speed: 200})
+// .delete(1)
+// .options({speed: 45})
+// .pause(300)
+// .type('!')
